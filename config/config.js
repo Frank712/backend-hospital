ad090c29697b460edf2167f908ee5e2b7e3ab8cc
Frank712 <franksuriel_7@hotmail.com>
2019-01-13 00:59:32 -0600

    Added Google SignIn and authentication

--- config/config.js
+++ config/config.js
@@ -1,3 +1,9 @@
 
 module.exports.SEED = '/-Thi@s/mY%S;._.;e&#s';
+module.exports.CLIENT_ID = '952339169567-gj8edoakhhgairtov5h671q6silseqj4.apps.googleusercontent.com';
 
+    /*======================================*/
+    /*|         url docs POSTMAN           |*/
+    /*======================================*/
+
+//  https://documenter.getpostman.com/view/6192337/RznHJdPb
--- routes/login.js
+++ routes/login.js
@@ -7,9 +7,110 @@ let app = express();
 let bcrypt = require( 'bcryptjs');
 let jwt = require('jsonwebtoken');
     /*======================================*/
+    /*|         Google Requires            |*/
+    /*======================================*/
+const CLIENT_ID = require('../config/config').CLIENT_ID;
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+    /*======================================*/
     /*|            Models                  |*/
     /*======================================*/
 const User = require('../models/user');
+    /*======================================*/
+    /*|         Login Google               |*/
+    /*======================================*/
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        name: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+app.post( '/google', async(req, res) =>{
+    let token = req.body.token;
+    let googleUser = await verify(token)
+        .catch( err =>{
+            return res.status(403).json({
+                ok: false,
+                message: 'Invalid token',
+                error: err
+            });
+        });
+    console.log(googleUser);
+
+    User.findOne({email: googleUser.email}, (err, userDB)=>{
+        if ( err  ){
+            return res.status(500).json({
+                ok: false,
+                message: 'An error has been occurred!',
+                error: err
+            });
+        }
+        if( userDB ){
+            if( userDB.google === false ){
+                return res.status(400).json({
+                    ok: false,
+                    message: 'You must be use a normal authentication'
+                });
+            } else {
+                //  Generating a owner token
+                let token = jwt.sign({
+                    user: userDB
+                }, SEED, {expiresIn: 14400}); // 4 hours for expiration
+                //  Send a response ok
+                res.json({
+                    ok: true,
+                    message: 'User login!',
+                    userDB,
+                    token
+                });
+            }
+        } else {
+            let user = new User({
+                name: googleUser.name,
+                lastname: googleUser.name,
+                email: googleUser.email,
+                img: googleUser.img,
+                password: '>:/',
+                google: true
+            });
+            console.log(user);
+            user.save((err, userSaved) =>{
+                if( err ){
+                    return res.status(500).json({
+                        ok: false,
+                        message: 'An error has been occurred while try to save a google user!',
+                        error: err
+                    });
+                }
+                //  Generating a owner token
+                let token = jwt.sign({
+                    user: userSaved
+                }, SEED, {expiresIn: 14400}); // 4 hours for expiration
+                //  Send a response ok
+                res.json({
+                    ok: true,
+                    message: 'User saved successfully!',
+                    userSaved,
+                    token
+                });
+            });
+        }
+    });
+});
 
 app.post( '/', (req, res) =>{
     let body = req.body;
@@ -48,7 +149,7 @@ app.post( '/', (req, res) =>{
             message: 'User login!',
             userDB,
             token
-        })
+        });
     });
 });
 
