77d99efaeeafc71db2e3fd2cdb80a967409186b1
Frank712 <franksuriel_7@hotmail.com>
2019-01-28 18:55:47 -0600

    Changes on config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 
-module.exports.SEED = '/-Thi@s/mY%S;._.;e&#s';
-module.exports.CLIENT_ID = '952339169567-gj8edoakhhgairtov5h671q6silseqj4.apps.googleusercontent.com';
+module.exports.SEED = process.env.SEED || '123456';
+module.exports.CLIENT_ID = process.env.CLIENT_ID || '952339169567-gj8edoakhhgairtov5h671q6silseqj4.apps.googleusercontent.com';
 
     /*======================================*/
     /*|         ENVIROMENT             |*/
